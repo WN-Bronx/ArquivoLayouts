9f3258c935c834331209dcc4b466a60f5a1b7c47
WN-Bronx <dj.pretto@hotmail.com>
2020-06-11 21:12:11 -0300

    Initial commit

---
+++ app/src/main/java/org/senac/arquivolayout/activity_layout_5.kt
@@ -0,0 +1,11 @@
+package org.senac.arquivolayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_layout_5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_5)
+    }
+}
